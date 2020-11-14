9f870df89f2a60e1f45aa674c1dabb5bd5b1b845
amandnj <54900338+amandnj@users.noreply.github.com>
2020-11-14 18:09:17 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# homework6
+# homework_6
+Starter code for Homework 6 - Homework description can be found at: https://docs.google.com/document/d/1G7BvRqTUewblgt4XjKh0zTwvbxvv1r1jQUa55Y692_c/edit?usp=sharing
---
+++ js/video.js
@@ -0,0 +1,14 @@
+var video;
+
+window.addEventListener("load", function() {
+	video = document.querySelector("#myVideo");
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	
+});
+
