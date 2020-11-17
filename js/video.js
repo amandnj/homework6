522c0e8d7cb6eab616ea1f16d08b46159f9c8f26
amandnj <54900338+amandnj@users.noreply.github.com>
2020-11-17 16:30:48 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,14 +1,30 @@
-var video;
+var Video
 
 window.addEventListener("load", function() {
 	video = document.querySelector("#myVideo");
 });
 
-document.querySelector("#play").addEventListener("click", function() {
+document.getElementById('#play').addEventListener("click", function(){
+	myVideo.play();
 	console.log("Play Video");
 });
 
-document.querySelector("#pause").addEventListener("click", function() {
-	
+document.getElementById('#pause').addEventListener("click", function(){
+	myVideo.pause();
+	console.log("Pause Video");
 });
 
+document.getElementById('#faster').addEventListener("click", function(){
+	myVideo.playbackRate = 2.0;
+	console.log("Speed Up");
+});
+
+document.getElementById('#slower').addEventListener("click", function(){
+	myVideo.playbackRate = 0.5;
+	console.log("Slow Down");
+});
+
+document.getElementById('#skip').addEventListener("click", function(){
+	myVideo.currentTime(myVideo.currentTime() + 15);
+	console.log("Skip Ahead");
+});
