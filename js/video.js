96a2b1c531d989f7e2d486d98387a15822412ced
amandnj <54900338+amandnj@users.noreply.github.com>
2020-11-18 14:43:58 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -5,10 +5,10 @@ var slowerbutton = document.getElementById("slower");
 var fasterbutton = document.getElementById("faster");
 var skipbutton = document.getElementById("skip");
 var mutebutton = document.getElementById("mute");
-var currentVol = document.getElementById("volume")
 var oldbutton = document.getElementById("old");
-var originalbutton = document.getElementById("original")
+var originalbutton = document.getElementById("original");
 var volslider = document.getElementById("volumeSlider")
+var currentvolume = document.querySelector("myVideo").volume;
 
 
 window.addEventListener("load", function() {
@@ -49,15 +49,12 @@ mutebutton.addEventListener("click", function(){
 });
 
 
-document.getElementById("volume").addEventListener("volumechange", function(){
-	video.volume();
-	document.getElementById("volume").innerHTML(video.volume());
-	console.log(video.volume());
-
+document.getElementById("volume").addEventListener("click", function(){
+	currentvolume.innerHTML(currentvolume);
 });
 
 originalbutton.addEventListener("click", function(){
-	video.load();
+	document.getElementById("myVideo");
 });
 
 oldbutton.addEventListener("click", function(){
