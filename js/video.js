faca77d0233a3665d6df3b3a20160e3fa5b719b4
amandnj <54900338+amandnj@users.noreply.github.com>
2020-11-18 12:04:44 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -5,6 +5,9 @@ var slowerbutton = document.getElementById("slower");
 var fasterbutton = document.getElementById("faster");
 var skipbutton = document.getElementById("skip");
 var mutebutton = document.getElementById("mute");
+var currentVol = document.getElementById("volume")
+var oldbutton = document.getElementById("old");
+var originalbutton = document.getElementById("original")
 
 
 window.addEventListener("load", function() {
@@ -13,7 +16,7 @@ window.addEventListener("load", function() {
 
 playbutton.addEventListener("click", function() {
 	video.play();
-	console.log("Played Video");
+	console.log(currentVol);
 });
 
 pausebutton.addEventListener("click", function() {
@@ -22,27 +25,58 @@ pausebutton.addEventListener("click", function() {
 });
 
 slowerbutton.addEventListener("click", function(){
-	video.playbackRate = 1.0;
+	video.playbackRate = 10%;
 	console.log("Slow Down");
 });
 
 fasterbutton.addEventListener("click", function(){
-	video.playbackRate = 2.0;
-	console.log("Speed Up");
+	video.playbackRate = 10%;
+	console.log(video.playbackRate);
 });
 
 skipbutton.addEventListener("click", function(){
-	video.currentTime += 15;
-	console.log("Skip Ahead");
+	video.currentTime(video.currentTime() + 5);
+	console.log(video.currentTime);
 });
 
 mutebutton.addEventListener("click", function(){
-	
-	if (video.muted = false){
-		video.muted=true;
+	if(video.muted){
+		video.muted = false;
+		mutebutton.innerHTML = "Mute";
+	} 
+	else {
+		video.muted = true;
+		mutebutton.innerHTML = "Unmute";
 	}
-	else (video.muted = true){
-		video.muted=false;
-	}
-	console.log("Muted")
+});
+
+var volumeControl = document.getElementById("volumeSlider");
+var setVolume = function(){
+	currentVol.innerHTML=(video.volume = this.value / 100;)
+};
+volumeControl.addEventListener('change',setVolume);
+volumeControl.addEventListener('input',setVolume);
+
+var bgCanvas= document.createElement("canvas");
+function makeItGrey() {
+    bgContext.drawImage(video, 0, 0, w, h);
+    var pixelData = bgContext.getImageData(0, 0, w, h);
+    for (var i = 0; i < pixelData.data.length; i += 4 ) {
+        var r = pixelData.data[i];
+        var g = pixelData.data[i+1];
+        var b = pixelData.data[i+2];
+        var averageColour = (r + g + b) / 3;
+        pixelData.data[i] = averageColour;
+        pixelData.data[i+1] = averageColour;
+        pixelData.data[i+2] = averageColour;
+    }
+    context.putImageData(pixelData, 0, 0);
+}
+
+oldbutton.addEventListener("click", function() {
+    video.setInterval(makeItGrey(), 33);
+}, false);
+
+originalbutton.addEventListener("click", function(){
+	document.getElementById(video).load();
 });
